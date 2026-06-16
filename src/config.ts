9a35c304c94dd23c8d74de8dac22c4349bfd5ee4
ShirokoLEET <2498469253@qq.com>
2026-06-16 13:53:35 +0800

    update a custom favcion

--- src/config.ts
+++ src/config.ts
@@ -10,7 +10,7 @@ import { LinkPreset } from "./types/config";
 export const siteConfig: SiteConfig = {
 	title: "Shiroko's Blog",
 	subtitle: "雪之泪 雨之音",
-	lang: "en", // Language code, e.g. 'en', 'zh_CN', 'ja', etc.
+	lang: "zh_CN", // Language code, e.g. 'en', 'zh_CN', 'ja', etc.
 	themeColor: {
 		hue: 0, // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
 		fixed: false, // Hide the theme color picker for visitors
@@ -31,11 +31,22 @@ export const siteConfig: SiteConfig = {
 	},
 	favicon: [
 		// Leave this array empty to use the default favicon
-		// {
-		//   src: '/favicon/icon.png',    // Path of the favicon, relative to the /public directory
-		//   theme: 'light',              // (Optional) Either 'light' or 'dark', set only if you have different favicons for light and dark mode
-		//   sizes: '32x32',              // (Optional) Size of the favicon, set only if you have favicons of different sizes
-		// }
+		{
+			src: "/favicon/favcion-32.png",
+			sizes: "32x32",
+		},
+		{
+			src: "/favicon/favcion-128.png",
+			sizes: "128x128",
+		},
+		{
+			src: "/favicon/favcion-180.png",
+			sizes: "180x180",
+		},
+		{
+			src: "/favicon/favcion-192.png",
+			sizes: "192x192",
+		},
 	],
 };
 
