eff23b851a5f6eb71f9d819d58f8ba51f855681b
ShirokoLEET <2498469253@qq.com>
2026-06-20 23:24:54 +0800

    add blog links to api

--- src/pages/api/blogs.json.ts
+++ src/pages/api/blogs.json.ts
@@ -1,10 +1,12 @@
 import { getSortedPosts } from "@utils/content-utils";
+import { getPostUrlBySlug } from "@utils/url-utils";
 
 export async function GET() {
 	const posts = await getSortedPosts();
 	const blogs = posts.map((post) => ({
 		title: post.data.title,
 		description: post.data.description,
+		link: new URL(getPostUrlBySlug(post.slug), import.meta.env.SITE).href,
 	}));
 
 	return new Response(
