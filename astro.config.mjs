324ab71922caf34424658953e71d0ffdec408cf2
ShirokoLEET <2498469253@qq.com>
2026-06-20 22:40:19 +0800

    update site domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -26,7 +26,7 @@ import { pluginCustomCopyButton } from "./src/plugins/expressive-code/custom-cop
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://sayonara.world/",
+	site: "https://blog.sayonara.world/",
 	base: "/",
 	trailingSlash: "always",
 	integrations: [
